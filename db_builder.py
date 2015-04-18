75810394b5893ad356995055f6dc133bcdcf9f77
pxtang <petertangisme@gmail.com>
2015-04-18 12:17:41 -0700

    Code to get URL of pokemon
    +test to make sure URL is correct

--- db_builder.py
+++ db_builder.py
@@ -5,7 +5,19 @@ MAX_POKE_NUM = 721 # get this from bulbapedia
 S_URL_BEGIN = "http://serebii.net/pokedex-dp/"
 S_URL_END = ".shtml"
 
+def get_url(num):
+  if (num < 10):
+    num_str = "00" + str(num)
+  elif (num < 100):
+    num_str = "0" + str(num)
+  else:
+    num_str = str(num)
+
+  return S_URL_BEGIN+num_str+S_URL_END
+
 def main():
+  curr_url = get_url(1)
+  print curr_url
 
 
 if __name__ == '__main__':
