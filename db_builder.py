8416ae8106e535f3369ff26e6a5ae8e85b2ffd5e
pxtang <petertangisme@gmail.com>
2015-04-18 12:11:53 -0700

    import and vars
    Set global vars and import packages

---
+++ db_builder.py
@@ -0,0 +1,12 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+MAX_POKE_NUM = 721 # get this from bulbapedia
+S_URL_BEGIN = "http://serebii.net/pokedex-dp/"
+S_URL_END = ".shtml"
+
+def main():
+
+
+if __name__ == '__main__':
+  main()
