4c9c3cb6ad3d81173139a1d68c2952db9a44aed4
pxtang <petertangisme@gmail.com>
2015-04-18 22:13:45 -0700

    Build function to rip source

--- db_builder.py
+++ db_builder.py
@@ -1,8 +1,10 @@
-import urllib2
+import urllib2, io
+import sys
 from bs4 import BeautifulSoup
 
 MAX_POKE_NUM = 721 # get this from bulbapedia
 S_URL_BEGIN = "http://serebii.net/pokedex-dp/"
+BEGIN_LEN = 30 # to avoid recalc
 S_URL_END = ".shtml"
 
 def get_url(num):
@@ -15,9 +17,23 @@ def get_url(num):
 
   return S_URL_BEGIN+num_str+S_URL_END
 
+def save_html(num):
+  curr_url = get_url(num)
+  print "**** opening %s" % curr_url
+
+  curr_page = urllib2.urlopen(curr_url)
+  soup = BeautifulSoup(curr_page.read())
+
+  fname = "html/%s" % curr_url[BEGIN_LEN:]
+
+  s_out = io.open(fname, "w", encoding = 'utf8')
+  s_out.write(soup.prettify())
+  s_out.close()
+  print "done writing %s to s_out" % fname
+
 def main():
-  curr_url = get_url(1)
-  print curr_url
+  for i in range(16,21):
+    save_html(i)
 
 
 if __name__ == '__main__':
