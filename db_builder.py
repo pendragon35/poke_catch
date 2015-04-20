40d52e81caeb8b843ef40c9947fddfe543abcbb3
pxtang <petertangisme@gmail.com>
2015-04-19 17:03:26 -0700

    fix for xy pokedex
    don’t know if they’re different?

--- db_builder.py
+++ db_builder.py
@@ -3,10 +3,10 @@ import sys
 from bs4 import BeautifulSoup
 
 MAX_POKE_NUM = 721 # get this from bulbapedia
-S_URL_BEGIN = "http://serebii.net/pokedex-dp/"
-BEGIN_LEN = 30 # to avoid recalc
+S_URL_BEGIN = "http://www.serebii.net/pokedex-xy/"
+BEGIN_LEN = len(S_URL_BEGIN) # to avoid recalc later
 S_URL_END = ".shtml"
-SOUP_LIM = 10
+SOUP_LIM = 9
 
 def get_url(num):
   if (num < 10):
@@ -49,9 +49,10 @@ def make_dicts(num,source,outfile):
   if (source == '-l'):
     curr_html.close()
 
-  poke_name = soup.title.string.split(' ')[-1].lower().encode("ascii",'ignore')
 
   fooinfos = soup.findAll("td","fooinfo",limit=SOUP_LIM)
+  poke_name = fooinfos[1].string.encode("ascii",'ignore').lower()
+
   catch_rate = int(fooinfos[-1].string)
 
   # print "fooinfos for %s" % poke_name
@@ -69,30 +70,40 @@ def main(argv):
   if (len(argv) == 1):
     print_usage()
     sys.exit()
-  elif (argv[1] == "-s" and len(argv) == 4):
+  elif (argv[1] == "-s" and len(argv) >= 4):
     for i in range(int(argv[2]),int(argv[3])+1):
       save_html(i)
   elif (argv[1] == "-d" and len(argv) == 5):
-    with open("table.txt","a+") as outfile:
-      #check if last line is > start or end
-      outfile.seek(-100,1)
-      last = outfile.readlines()[-1]
+    start = int(argv[2])
+    end = int(argv[3])+1
 
-      last_num = int(last.split(',')[0])
-      start = int(argv[2])
-      end = int(argv[3])+1
+    with open("table_xy.txt","a+") as outfile:
+      # check if last line is > start or end
 
-      if (last_num == MAX_POKE_NUM or start > MAX_POKE_NUM):
-        print "all already pokemon imported!"
-      if (end > MAX_POKE_NUM + 1):
-        end = MAX_POKE_NUM + 1
+      try:
+        outfile.seek(-2, 2)            # Jump to the second last byte.
+        while outfile.read(1) != "\n": # Until EOL is found...
+          outfile.seek(-2, 1)        # ...jump back the read byte plus one more.
+        last = outfile.readline()      # Read last line.
 
-      if (last_num >= start):
-        start = last_num + 1
-      if (last_num >= end - 1):
-        print "Error: table exists past specified end number"
-        print "Now quitting..."
-        sys.exit()
+        # outfile.seek(-100,1)
+        # last = outfile.readlines()[-1]
+        last_num = int(last.split(',')[0])
+
+        if (last_num == MAX_POKE_NUM or start > MAX_POKE_NUM):
+          print "all already pokemon imported!"
+        if (end > MAX_POKE_NUM + 1):
+          end = MAX_POKE_NUM + 1
+
+        if (last_num >= start):
+          start = last_num + 1
+        if (last_num >= end - 1):
+          print "Error: table exists past specified end number"
+          print "Now quitting..."
+          sys.exit()
+      except IOError: #file didn't exit to read last line, skip that
+        print "Making new file... If you didn't start at 1, rm and run again starting at 1"
+        pass
 
       for i in range(start,end):
         make_dicts(i, argv[4], outfile)
