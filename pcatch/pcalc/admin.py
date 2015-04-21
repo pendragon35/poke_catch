d0c193444b5fa3fa10665e58ec58092a5b810c73
pxtang <petertangisme@gmail.com>
2015-04-20 21:38:36 -0700

    starting to add django views

--- pcatch/pcalc/admin.py
+++ pcatch/pcalc/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Rates
+
+class RatesAdmin(admin.ModelAdmin):
+  list_display = ('poke_num','poke_name','dp_rate','bw_rate','xy_rate')
+  search_fields = ['poke_name']
+
+admin.site.register(Rates, RatesAdmin)
--- pcatch/pcatch/urls.py
+++ pcatch/pcatch/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'pcatch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^pcalc/', include('pcalc.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
